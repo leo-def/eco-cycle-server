7c116612ded767a250da521ba664ae19ed4c0ae8
Leonardo de Freitas Oliveira <leo_defoliveira@live.com>
2022-12-30 20:03:24 -0300

    Remove tests

--- src/api/api-group/middlewares/group.middleware.spec.ts
+++
@@ -1,7 +0,0 @@
-import { GroupMiddleware } from './group.middleware';
-
-describe('GroupMiddleware', () => {
-  it('should be defined', () => {
-    expect(new GroupMiddleware(null)).toBeDefined();
-  });
-});
