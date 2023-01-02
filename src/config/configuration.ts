a378d745200d9b7a80e67b948916bba2e158948b
Leonardo de Freitas Oliveira <leo_defoliveira@live.com>
2023-01-02 02:34:31 -0300

    EnvVars: Add env var DATABASE_NAME

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -5,7 +5,7 @@ export const configuration = () => ({
   [EnvVarsEnum.PORT]: parseInt(process.env.PORT, 10) || 3000,
   [EnvVarsEnum.DATABASE_CONFIG]: {
     type: process.env.DATABASE_TYPE,
-    database: process.env.DATABASE_URL,
+    database: (process.env.DATABASE_URL || process.env.DATABASE_NAME),
     host: process.env.DATABASE_HOST,
     port: process.env.DATABASE_PORT,
     username: process.env.DATABASE_USERNAME,
