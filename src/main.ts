0b5a422cd04c80c9728e04d90316dc79d5e60878
Leonardo de Freitas Oliveira <leo_defoliveira@live.com>
2023-01-08 17:53:43 -0300

    Package: add lib helmet

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
+import helmet from 'helmet';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.use(helmet());
   await app.listen(3000);
 }
 bootstrap();
